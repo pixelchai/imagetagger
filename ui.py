231b4600968417c5f4b38094fdfe8c5022d7529a
pixel0a <pixel00a@gmail.com>
2019-12-15 16:03:03 +0000

    UI design

--- ui.py
+++ ui.py
@@ -1,5 +1,44 @@
 from qtpy import QtWidgets, QtGui
-from qtpy.QtCore import Qt, QTimer
+from qtpy.QtWidgets import QSizePolicy
+from qtpy.QtCore import Qt
+
+class CentralWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored,
+                                            QtWidgets.QSizePolicy.Ignored)
+        size_policy.setHorizontalStretch(0)
+        size_policy.setVerticalStretch(0)
+        self.setSizePolicy(size_policy)
+
+        self.image_label = QtWidgets.QLabel(self)
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored,
+                                            QtWidgets.QSizePolicy.Ignored)
+        size_policy.setHorizontalStretch(0)
+        size_policy.setVerticalStretch(1)
+        self.image_label.setSizePolicy(size_policy)
+        self.image_label.setPixmap(QtGui.QPixmap("res/loading.png"))
+        self.image_label.setAlignment(Qt.AlignCenter)
+        self.image_label.setContentsMargins(0, 0, 0, 0)
+        self.layout.addWidget(self.image_label, 0, Qt.AlignHCenter | Qt.AlignVCenter)
+
+        self.button_holder = QtWidgets.QWidget(self)
+        self.button_holder.layout = QtWidgets.QHBoxLayout(self.button_holder)
+
+        btn = QtWidgets.QPushButton(self.button_holder)
+        btn.setText("wow")
+        self.button_holder.layout.addWidget(btn)
+
+        btn = QtWidgets.QPushButton(self.button_holder)
+        btn.setText("wow2")
+        self.button_holder.layout.addWidget(btn)
+
+        self.layout.addWidget(self.button_holder, 0, Qt.AlignBottom)
+
 
 class Tagger(QtWidgets.QMainWindow):
     def __init__(self):
@@ -7,7 +46,7 @@ class Tagger(QtWidgets.QMainWindow):
 
         self.setWindowTitle("ImageTagger")
 
-        self.central_widget = QtWidgets.QWidget(self)
+        self.central_widget = CentralWidget(self)
         self.setCentralWidget(self.central_widget)
 
         self.resize(960, 540)
